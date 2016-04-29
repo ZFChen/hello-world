c4e34b001d2a335106e166638cc78d359e1157c6
陈志方 <313509111@qq.com>
2016-04-29 21:49:56 +0800

    use Xcode, first  commit to github.

---
+++ github_xcode_demo/github_xcode_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github_xcode_demo
+//
+//  Created by ZFChen on 16/4/29.
+//  Copyright © 2016年 CS193p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github_xcode_demo/github_xcode_demoTests/github_xcode_demoTests.m
@@ -0,0 +1,39 @@
+//
+//  github_xcode_demoTests.m
+//  github_xcode_demoTests
+//
+//  Created by ZFChen on 16/4/29.
+//  Copyright © 2016年 CS193p. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface github_xcode_demoTests : XCTestCase
+
+@end
+
+@implementation github_xcode_demoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
